ae1dc6eeb275e53f836493c61a5b0ce36b86784e
Yuri Gomes <yurig.favero@gmail.com>
2024-02-13 12:43:20 -0300

    Push 2

--- lib/container/container_all.dart
+++ lib/container/container_all.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'package:flutter/material.dart';
 
 class ContainerAll extends StatelessWidget {
@@ -18,25 +17,4 @@ class ContainerAll extends StatelessWidget {
       ),
     );
   }
-=======
-import 'package:flutter/material.dart';
-
-class ContainerAll extends StatelessWidget {
-  Widget child;
-  
-  ContainerAll({super.key, required this.child});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: this.child,
-      margin: EdgeInsets.all(15),
-      padding: EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        color: Color.fromRGBO(235, 229, 229, 0.5),
-        borderRadius: BorderRadius.circular(20)
-      ),
-    );
-  }
->>>>>>> c6b14415f315888bbd043e95ee4cf3a615859717
 }
--- lib/user/user.dart
+++ lib/user/user.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class User {
   String name;
   String email;
@@ -10,16 +9,3 @@ class User {
     required this.password,
   });
 }
-=======
-class User {
-  String name;
-  String email;
-  String password;
-  
-  User({
-    required this.name,
-    required this.email,
-    required this.password,
-  });
-}
->>>>>>> c6b14415f315888bbd043e95ee4cf3a615859717
